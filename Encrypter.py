818798bf1836414bae3bcb53e1170259dab07d03
Yiu Chung Yau (Tommy) <wwkb1233@gmail.com>
2018-08-02 00:11:13 -0400

    fixed bugs

--- Encrypter.py
+++ Encrypter.py
@@ -1,16 +1,19 @@
 
 #!/usr/bin/env python
 
-import seccure
+import zipimport
+
+importer = zipimport.zipimporter('seccure.mod')
+seccure = importer.load_module('seccure')
 
 def encrypt(plaintext,private_key):
-	public_key = str(seccure.passphrase_to_pubkey(bytes(private_key,encoding='utf-8')))
-	ciphertext = seccure.encrypt(bytes(plaintext,encoding='utf-8'),bytes(public_key,encoding='utf-8'))
+	public_key = str(seccure.passphrase_to_pubkey(bytes(private_key.encode('utf-8'))))
+	ciphertext = seccure.encrypt(bytes(plaintext.encode('utf-8')),bytes(public_key.encode('utf-8')))
 	return ciphertext
 
 def decrypt(ciphertext,private_key):
-	plaintext = seccure.decrypt(ciphertext,bytes(private_key,encoding='utf-8'))
-	return(plaintext)
+	plaintext = seccure.decrypt(ciphertext,bytes(private_key.encode('utf-8')))
+	return plaintext
 
 if __name__ == "__main__":
-	print(decrypt(encrypt("tommy","mykey"),"mykey").decode('utf-8'))
+	print (decrypt(encrypt("tommy","mykey"),"mykey").decode('utf-8'))
